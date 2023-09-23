ebaab1b0077c1199a404cf48815bf0eeced82ca9
Yasu-G <140491519+Yasu-G@users.noreply.github.com>
2023-09-24 07:47:09 +0900

    aaa

---
+++ aaa.php
@@ -0,0 +1,62 @@
+<div class="news_posts_small">
+                       <div class="row">
+                         <div class="col-lg-2 col-md-2 col-sx-12">
+                           <div class="calendar_news_border">
+                             <div class="calendar_news_border_1">
+                               <div class="calendar_month">OCT</div>
+                               <div class="calendar_day">
+                                 <span>15</span>
+                               </div>
+                             </div>
+                           </div>
+                         </div>
+                         <div class="col-lg-10 col-md-10 col-sx-12">
+                           <div class="news_post_small_title">
+                             <a href="news_detail.html">AWSハンズオンセミナー</a>
+                           </div>
+                           <div class="news_post_meta">
+                             <ul>
+                               <li>
+                                 10月8日　新宿〇〇ビル8Fにて、AWSハンズオンセミナーを開催します。AWSを実際に構築してみます。参加希望者は...
+                               </li>
+                             </ul>
+                           </div>
+                           <div class="read_continue">
+                             <button>詳細を見る</button>
+                           </div>
+                         </div>
+                       </div>
+                       <hr />
+                     </div>
+                     <!-- news loop until here-->
+ 
+                     <div class="news_posts_small">
+                       <div class="row">
+                         <div class="col-lg-2 col-md-2 col-sx-12">
+                           <div class="calendar_news_border">
+                             <div class="calendar_news_border_1">
+                               <div class="calendar_month">OCT</div>
+                               <div class="calendar_day">
+                                 <span>15</span>
+                               </div>
+                             </div>
+                           </div>
+                         </div>
+                         <div class="col-lg-10 col-md-10 col-sx-12">
+                           <div class="news_post_small_title">
+                             <a href="news_detail.html">AWSハンズオンセミナー</a>
+                           </div>
+                           <div class="news_post_meta">
+                             <ul>
+                               <li>
+                                 10月8日　新宿〇〇ビル8Fにて、AWSハンズオンセミナーを開催します。AWSを実際に構築してみます。参加希望者は...
+                               </li>
+                             </ul>
+                           </div>
+                           <div class="read_continue">
+                             <button>詳細を見る</button>
+                           </div>
+                         </div>
+                       </div>
+                       <hr />
+                     </div>
