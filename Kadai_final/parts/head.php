e848c9e1becabcc6f6066aa1c3a3bd3cf2886d53
Yasu-G <140491519+Yasu-G@users.noreply.github.com>
2023-10-07 07:58:44 +0900

    Kadai＿final1007

---
+++ Kadai_final/parts/head.php
@@ -0,0 +1,27 @@
+<head>
+  <title>SAMURAI整体院</title>
+  <meta name="description" content="SAMURAI整体院の「当院について」ページです">
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style-head.css">
+
+  <!-- フォントの読み込み -->
+    <!-- Shippori Mincho -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Shippori+Mincho:wght@600&display=swap" rel="stylesheet">
+    <!--  Merriweather Sans -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Merriweather+Sans&family=Shippori+Mincho:wght@600&display=swap" rel="stylesheet">
+    <!-- Noto Sans JP -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&family=Merriweather+Sans&family=Noto+Sans+JP&family=Shippori+Mincho:wght@600&display=swap" rel="stylesheet">
+
+  <!-- slickの読み込み -->
+      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css">
+      <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+
+</head>
---
+++ Kadai_final/parts/header.php
@@ -0,0 +1,14 @@
+
+<link rel="stylesheet" href="../css/style-header.css">
+
+<header>
+  <img id="logo" src="../img/Frame 2259.png" alt="logo">
+  <nav id="nav-pc">
+    <a href="index.thtml">ホーム </a>
+    <a href="about.html">当院について </a> 
+    <a href="course.html">コース・料金 </a> 
+    <a href="news.html">お知らせ </a>
+    <a href="voice.html">お客様の声 </a>
+    <a id="reservation" href="contact-thanks.html">ご予約・お問合せ </a>
+  </nav>
+</header>
